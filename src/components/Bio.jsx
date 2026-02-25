6b5716703d7946359c2e8e39ee946867a1d0eaed
Deacon Sham <deaconsham@icloud.com>
2026-02-24 23:01:21 -0500

    updated bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -6,7 +6,7 @@ function Bio() {
             </p>
             <p>
                 My interest in tech sparked from a curiosity about physics and how the world functions which led me into engineering here at Waterloo.
-                I am interested in all things embedded and machine learning while I enjoy playing badminton on the side.
+                I am interested in all things embedded and machine learning, while I enjoy playing badminton and baking on the side.
             </p>
         </section>
     )
