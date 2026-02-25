cf0a3ea97a8b1b8e5ba7b4e4b37ad55d5da5e76e
Deacon Sham <deaconsham@icloud.com>
2026-02-24 22:13:38 -0500

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 function Footer() {
     return (
         <footer className="footer">
+            <p>Built with React.js and Vite, design inspired by the TTC.</p>
             <p>© 2026 Deacon Sham</p>
         </footer>
     )
