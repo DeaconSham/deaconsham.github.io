f5fabe27c6ff6eea1a85b445c7b6794cc0281843
Deacon Sham <deaconsham@icloud.com>
2026-02-26 10:52:12 -0500

    added credit for font use

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="footer">
             <p>Built with React.js and Vite, design inspired by the TTC.</p>
-            <p>Toronto Subway Font is a trademark of Quadrat Communications.</p>
+            <p>Toronto Subway font is a trademark of Quadrat Communications.</p>
             <p>© 2026 Deacon Sham</p>
         </footer>
     )
